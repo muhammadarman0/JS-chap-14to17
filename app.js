d3e704e15e7b34db2173f31110fb29c2e06da2cb
armancoder15 <armandevs74@gmail.com>
2025-10-29 00:59:01 -0700

    Solve chapter 14 t0 17

---
+++ app.js
@@ -0,0 +1,134 @@
+// console.log("Javascript --> runing");
+
+
+
+// Question no 1
+// var student = [];
+// student.push("Arman","Ahmed","Ali","Hasan")
+// console.log(student);
+
+// Question no 2
+
+// let obj = [true,["karachi"],"islamabad",12]
+
+// console.log(obj);
+
+
+// Question no 3
+
+
+// let arr = ["karachi", "lahore", "islamabad", "quetta"];
+
+// console.log(arr);
+
+// // Question no 4
+
+// let numArr = [2, 4, 6, 8, 10, 12, 14];
+// console.log(numArr);
+
+// // Question no 5
+
+// let BooleanArr = [true, false];
+
+// console.log(BooleanArr);
+
+// Question no 6
+
+// let MixedArr = ["Karachi", 804, true, "Lahore", 102, false["islamabad", 2023, true]]
+// console.log(MixedArr);
+
+// Question no 7
+
+
+// let Edu = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"]
+// document.writeln(Edu)
+
+// Question no 8
+
+// let stu = ["Ahmed", "Arman", "Samad"];
+
+// let obtMarks = [320, 230, 480]
+
+// let per = (obtMarks[0] / 500) * 100
+// let per2 = (obtMarks[1] / 500) * 100
+// let per3= (obtMarks[2] / 500) * 100
+
+
+// console.log("Score of ",stu[0],obtMarks[0],". Percentage is ",per,"%");
+// console.log("Score of ",stu[1],obtMarks[1],". Percentage is ",per2,"%");
+// console.log("Score of ",stu[2],obtMarks[2],". Percentage is ",per3,'%');
+
+// Question no 9
+
+// let color = ["red", "green", "blue"]
+// console.log(color);
+
+// color.unshift("pink")
+
+// console.log(color);
+
+// color.push("borwn")
+
+// console.log(color);
+
+// color.unshift("grey","purple")
+// console.log(color);
+
+// color.shift()
+// console.log(color);
+
+// color.pop()
+// console.log(color);
+
+// color.splice(0,3,"yellow")
+// console.log(color);
+
+// Question no 10
+
+// let score = [320,230,480,120]
+// document.writeln(score ,"<br>")
+// let scoreright = [120,230,320,480]
+// document.writeln(scoreright)
+
+// Question no 11
+
+
+// let city = ["karachi","lahore","islamabad","quetta"];
+
+// console.log("City list",city);
+
+// city[0,3];
+// console.log("Select city lists",city);
+
+// Question no 12
+// var Catarr = ["This", " is ", "my", "cat"];
+
+// document.writeln(Catarr,"<br>")
+
+// let string = " This is my cat"
+
+// document.writeln(string)
+
+// Question no 13
+
+
+// let firstInFirstOut = ["keyboard","mouse","printer","moniter"]
+
+// console.log(firstInFirstOut);
+
+// firstInFirstOut.shift()
+// console.log(firstInFirstOut.shift());
+
+// Question no 14
+
+// let lastInFirstOut = ["keyboard","mouse","printer","moniter"];
+
+// console.log(lastInFirstOut);
+
+// lastInFirstOut.pop();
+// console.log(lastInFirstOut);
+
+
+
+
+
